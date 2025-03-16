95522c0d9a8e8cf6d3def4c675d3a41e8615195c
Navya Neethi S <112698395+navyaneethi@users.noreply.github.com>
2025-03-16 10:24:12 +0530

    Create answer_one.py

---
+++ Pattern1/answer_one.py
@@ -0,0 +1,13 @@
+n = int(input())
+diff = 6
+num = 1
+print(num)
+for i in range(1,n):
+    num+=diff
+    print(num,end=" ")
+    diff-=1
+    prev = num
+    for j in range(i):
+        prev=prev-diff-j
+        print(prev,end=" ")
+    print()
